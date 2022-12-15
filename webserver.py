bd77481e635d37ea0271a0d65e593aa7b7370724
Naveenkumar4325 <naveenelumalai197@gmail.com>
2022-12-15 13:28:04 +0530

    First release of Web server code

--- webserver.py
+++ webserver.py
@@ -1,39 +1,3 @@
-# Developing a Simple Webserver
-
-# AIM:
-
-To develop a simple webserver to serve html programming pages.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-HTML content creation is done
-
-### Step 2:
-
-Design of webserver workflow
-
-### Step 3:
-
-
-
-### Step 4:
-
-Serving the HTML pages.
-
-base) sec@sec-HP-250-15-6-inch-G9-Notebook-PC:~/myproject/webserver$ python3 webserver.py
-my webserver is running....
-request received
-
-### Step 5:
-
-Testing the webserver
-
-
-## PROGRAM:
-Implementation using Python code
-
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <html>
@@ -60,11 +24,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running....")
 httpd.serve_forever()
-
-
-## OUTPUT:
-
-
-
-## RESULT:
-The program is executed succesfully
